a241960dca787627ad423ee227f9d3551517e777
Jonathan <jon8921@hotmail.com>
2023-08-26 15:04:11 -0500

    d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Your JavaScript code (index.js)
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const yourName = 'John'; // Replace 'John' with your actual name
+newHeader.textContent = `${yourName} is the champion`;
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.replaceWith(newHeader);
+}
